00a4c13696ac7001624e23d04870a1477460ed41
Marcela Ishihara <106243581+cell-ai@users.noreply.github.com>
2025-05-07 16:12:33 -0300

    Update read_tables.py

--- read_tables.py
+++ read_tables.py
@@ -4,29 +4,27 @@ def diamond(file):
   '''
   processes to df, a file generated from diamond blastp
   '''
-  df = pd.read_csv(file, sep = "\t", header = None, names = ["qseqid", "qlen", "sseqid", "slen", "salltitles", "pident", "length", "mismatch", 
+  diamond_df = pd.read_csv(file, sep = "\t", header = None, names = ["qseqid", "qlen", "sseqid", "slen", "salltitles", "pident", "length", "mismatch", 
   "gapopen", "qstart", "qend", "sstart", "send", "qcovhsp", "evalue", "bitscore"]
-  return df
+  return diamond_df
 
 
-def interproscan(file, go = True, pathway = True):
-  '''
-  processes to df, a file generated from interproscan search
-  '''
-  if go, pathway:
-  interpro_df = pd.read_csv("concat_ro.refseq_serine.fasta.tsv", sep = "\t", header = None,
-                           names = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
-                                   "e-value", "type", "date", "ipr", "ipr_desc", "go", "pathway"])
-  else:
-    if go:
-      interpro_df = pd.read_csv("concat_ro.refseq_serine.fasta.tsv", sep = "\t", header = None,
-                           names = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
-                                   "e-value", "type", "date", "ipr", "ipr_desc", "go"])
-    if pathway:
-      interpro_df = pd.read_csv("concat_ro.refseq_serine.fasta.tsv", sep = "\t", header = None,
-                           names = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
-                                 "e-value", "type", "date", "ipr", "ipr_desc", "pathway"])
+def interproscan(file, go=True, pathway=True):
+    '''
+    Processes to df, a file generated from InterProScan search
+    '''
+    if go and pathway:
+        colnames = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
+                    "e-value", "type", "date", "ipr", "ipr_desc", "go", "pathway"]
+    elif go:
+        colnames = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
+                    "e-value", "type", "date", "ipr", "ipr_desc", "go"]
+    elif pathway:
+        colnames = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
+                    "e-value", "type", "date", "ipr", "ipr_desc", "pathway"]
     else:
-      interpro_df = pd.read_csv("concat_ro.refseq_serine.fasta.tsv", sep = "\t", header = None,
-                     names = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
-                           "e-value", "type", "date", "ipr", "ipr_desc"])
+        colnames = ["qseqid", "md5", "len", "db", "pfam_id", "pfam_desc", "start", "end",
+                    "e-value", "type", "date", "ipr", "ipr_desc"]
+    
+    interpro_df = pd.read_csv(file, sep="\t", header=None, names=colnames)
+    return interpro_df
